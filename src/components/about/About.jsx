6194f4f8d94486e42ec28cf5cb0b3ee4c2983051
Abdul Haseeb <work.ahaseeb@gmail.com>
2022-11-23 02:57:21 +0530

    built the about section and made it responsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import "./about.css"
+import ME from '../../assets/my-photo-2.JPG'
+import { FiAward } from 'react-icons/fi'
+import { GiBookPile } from 'react-icons/gi'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5> Get to know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FiAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years working experience</small>
+            </article>
+
+            <article className='about__card'>
+              <GiBookPile className='about__icon' />
+              <h5>Projects</h5>
+              <small>10+ projects using React & Angular</small>
+            </article>
+          </div>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap in</p>
+
+          <a href="#contact" className='btn btn-primary'></a>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,16 +8,25 @@ import { useState } from 'react'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#');
-
   return (
     <nav>
-      <a href="#" className={activeNav === '#' ? 'active' : ''}><FaHome /></a>
+      <a href="#" onClick={() => setActiveNav('home')} className={ activeNav === '#' ? 'active' : ''}><FaHome /></a>
       <a href="#about" onClick={() => setActiveNav('about')} className={ activeNav === 'about' ? 'active' : ''}><FaUser /></a>
-      <a href="#experience"><FaBook /></a>
-      <a href="#portfolio"><GoProject /></a>
-      <a href="#contact"><AiOutlineMessage /></a>      
+      <a href="#experience" onClick={() => setActiveNav('experience')} className={ activeNav === 'experience' ? 'active' : ''}><FaBook /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('portfolio')} className={ activeNav === 'portfolio' ? 'active' : ''}><GoProject /></a>
+      <a href="#contact" onClick={() => setActiveNav('contact')} className={ activeNav === 'contact' ? 'active' : ''}><AiOutlineMessage /></a>      
     </nav>
   )
 }
 
 export default Nav
+
+// import React, { Component } from 'react'
+
+// export default class Nav extends Component {
+//   render() {
+//     return (
+//       <div>Nav</div>
+//     )
+//   }
+// }
