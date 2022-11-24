a02d65a3779f12c93e189a24d2c193d72aec4f40
Abdul Haseeb <work.ahaseeb@gmail.com>
2022-11-24 15:22:28 +0530

    experience section in progress

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,11 +5,7 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@material-ui/lab/TabContext';
 import TabList from '@material-ui/lab/TabList';
 import TabPanel from '@material-ui/lab/TabPanel';
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { BsCodeSlash } from 'react-icons/bs'
 
 
 
@@ -45,51 +41,90 @@ const Experience = () => {
             </Box>
 
             <TabPanel value="1">
-              <Accordion expanded={expanded === 'panel1'} onChange={handleChangeAccordian('panel1')}>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1bh-content"
-                  id="panel1bh-header"
-                >
-                  <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                    General settings
-                  </Typography>
-                  <Typography>I am an accordion</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-                    Aliquam eget maximus est, id dignissim quam.
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-              <Accordion expanded={expanded === 'panel2'} onChange={handleChangeAccordian('panel2')}>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel2bh-content"
-                  id="panel2bh-header"
-                >
-                  <Typography sx={{ width: '33%', flexShrink: 0 }}>Users</Typography>
-                  <Typography sx={{ color: 'text.secondary' }}>
-                    You are currently not an owner
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
-                    varius pulvinar diam eros in elit. Pellentesque convallis laoreet
-                    laoreet.
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
+              <div className="experience__skills">
+                <div className="skill__content">
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Computer programming & coding</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Software Developement</h4>
+                    <small className='text-light'> description of software development</small>
+                    
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Object Oriented Design (OOD)</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Software testing and debugging</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+                  </article>
+                </div>
+              </div>
             </TabPanel>
 
             <TabPanel value="2">
-              Item Two
+            <div className="experience__skills">
+                <div className="skill__content">
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Computer programming & coding</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Software Developement</h4>
+                    <small className='text-light'> description of software development</small>
+                    
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Object Oriented Design (OOD)</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Software testing and debugging</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+                  </article>
+                </div>
+              </div>
             </TabPanel>
 
             <TabPanel value="3">
-              Item Three
+            <div className="experience__skills">
+                <div className="skill__content">
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Computer programming & coding</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Software Developement</h4>
+                    <small className='text-light'> description of software development</small>
+                    
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Object Oriented Design (OOD)</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+                  </article>
+                  <article className='skill__details'>
+                    <BsCodeSlash />
+                    <h4>Software testing and debugging</h4>
+                    <small className='text-light'> Proficient in algorithms and coding techniques.</small>
+                  </article>
+                </div>
+              </div>
             </TabPanel>
 
           </TabContext>
